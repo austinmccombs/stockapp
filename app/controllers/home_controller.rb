b3c08f38d862fa5069a0ce3d5d1510e74250ce46
Austin McCombs <austinmccombs22@gmail.com>
2020-12-29 14:17:35 -0700

    added functionality to the ticker form to return company name

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
   def index
     @api = StockQuote::Stock.new(api_key: 'pk_61becff1e4974bacba63b255c413dc6f')
+    if params[:ticker]
+      @stock = StockQuote::Stock.quote(params[:ticker])
+    end
   end
 
   def about
