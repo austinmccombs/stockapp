9154210a38c59efbb28022fc4026b646af8c34d6
Austin McCombs <austinmccombs22@gmail.com>
2020-12-29 18:49:16 -0700

    introducing the stocks db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_29_225819) do
+ActiveRecord::Schema.define(version: 2020_12_30_014014) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_stocks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
